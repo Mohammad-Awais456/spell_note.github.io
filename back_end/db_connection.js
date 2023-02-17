b8bcaa9d3081fa7bcfa8ec2f9a6f68a18cca2749
Mohammad Awais <Mohammad-Awais456@users.noreply.github.com>
2023-02-17 09:35:40 +0500

    initial commit

--- back_end/db_connection.js
+++ back_end/db_connection.js
@@ -1,6 +1,7 @@
 const mongoose= require("mongoose");
 
 let db=process.env.DataBase;
+console.log(db)
 // db="mongodb://localhost:27017/spell_notes";
 mongoose.connect(db,
     {useNewUrlParser: true ,useUnifiedTopology: true  }).then(()=>console.log("connection successful to DataBase"))

3e1c1b1534571d9719b29bdbc271ae1825d0cae6
Mohammad Awais <Mohammad-Awais456@users.noreply.github.com>
2023-02-17 12:13:53 +0500

    database file changed

--- back_end/db_connection.js
+++ back_end/db_connection.js
@@ -1,12 +1,31 @@
 const mongoose= require("mongoose");
 
 // let db=process.env.DataBase;
-let db="";
-db ="mongodb+srv://awais:3mwtZ6uQJIzRU0G4@cluster0.w3ct3.mongodb.net/Mern?retryWrites=true&w=majority";
-console.log(db)
+// let db="";
+// db ="mongodb+srv://awais:3mwtZ6uQJIzRU0G4@cluster0.w3ct3.mongodb.net/Mern?retryWrites=true&w=majority";
+// console.log(db)
 // db="mongodb://localhost:27017/spell_notes";
-mongoose.connect(db,
-    {useNewUrlParser: true ,useUnifiedTopology: true  }).then(()=>console.log("connection successful to DataBase"))
-.catch((e)=>console.log("no connection "+e.message));
+// mongoose.connect(db,{useNewUrlParser: true ,useUnifiedTopology: true  }).then(()=>console.log("connection successful to DataBase"))
+// .catch((e)=>console.log("no connection "+e.message));
+
+        // connect to database
+mongoose.set('strictQuery', true);
+const Connect_Database= async ()=> 
+{
+    try
+    {
 
         
+        let url = "mongodb+srv://awais:3mwtZ6uQJIzRU0G4@cluster0.w3ct3.mongodb.net/Mern?retryWrites=true&w=majority";
+        //  url = "mongodb://127.0.0.1:27017/Portfolio";
+        let res= await mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true});
+        console.log("Database connected successfully with server "+ res.connection.host);
+    }
+    catch(e)
+    {
+        console.log(e);
+    }
+
+}
+
+Connect_Database();
